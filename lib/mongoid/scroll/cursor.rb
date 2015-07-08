ba5f25f879ef8f0de796d10167c1825cc3cbe346
Borja GVO <borjavinuessa@gmail.com>
2015-07-08 19:45:28 +0200

    Much more simple than previous commit updates #3

--- lib/mongoid/scroll/cursor.rb
+++ lib/mongoid/scroll/cursor.rb
@@ -79,7 +79,8 @@ module Mongoid
           when 'BSON::ObjectId', 'Moped::BSON::ObjectId' then value
           when 'String' then value.to_s
           when 'Date' then Time.utc(value.year, value.month, value.day).to_i
-          when 'DateTime', 'Time' then add_msec_to_time(value)
+          when 'DateTime' then value.to_time.to_f
+          when 'Time' then value.to_f
           when 'Float' then value.to_f
           when 'Integer' then value.to_i
           else
